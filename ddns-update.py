7d59cb5ddebfe6a0385c702685b2b3f11346f2fb
guiyubj <yuguicode@gmail.com>
2017-01-31 13:14:01 -0500

    V1.1 added auto repeat

--- ddns-update.py
+++ ddns-update.py
@@ -1,31 +1,34 @@
 import requests
+import time
 
 str_user = "aaronk199"
 str_pwd = "223b0384556ef70e431309e513141335"
 str_ip_check = "http://ipinfo.io/ip"
 str_ip_update = "http://api.dynu.com/nic/update?"
+i_delay = 5 # seconds of delay between each refresh
 
-print("Getting current IP...")
-resp_ip = requests.get(str_ip_check)
+while True:
+    print("Getting current IP...")
+    resp_ip = requests.get(str_ip_check)
 
-if not resp_ip.status_code == 200:
-    raise "Cannot get IP addr from "+str_ip_check
-    pass
-else:
-    str_ip = resp_ip.text
-    print("IP: "+str_ip)
+    if not resp_ip.status_code == 200:
+        raise "Cannot get IP addr from "+str_ip_check
+        pass
+    else:
+        str_ip = resp_ip.text
+        print("IP: "+str_ip)
 
-print("Updating remote IP addr...")
-# dynu.com ip update api:
-# http://api.dynu.com/nic/update?hostname=[HOSTNAME]&alias=[ALIA]&myip=[IP ADDRESS]&myipv6=[IPv6 ADDRESS]&password=[PASSWORD or MD5(PASSWORD)]
+    print("Updating remote IP addr...")
+    # dynu.com ip update api:
+    # http://api.dynu.com/nic/update?hostname=[HOSTNAME]&alias=[ALIA]&myip=[IP ADDRESS]&myipv6=[IPv6 ADDRESS]&password=[PASSWORD or MD5(PASSWORD)]
 
-resp_update = requests.get(str_ip_update, params = {"myip":"67.250.40.10", "username":str_user, "password":str_pwd})
+    resp_update = requests.get(str_ip_update, params = {"myip":"67.250.40.10", "username":str_user, "password":str_pwd})
 
-if not resp_update.status_code == 200:
-    raise "Cannot update IP addr to "+str_ip_update
-    pass
-else:
-    str_ip = resp_update.text
-    print("Resp: "+resp_update.text)
+    if not resp_update.status_code == 200:
+        raise "Cannot update IP addr to "+str_ip_update
+        pass
+    else:
+        str_ip = resp_update.text
+        print("Resp: "+resp_update.text +'\n')
 
-input()
+    time.sleep(i_delay)
